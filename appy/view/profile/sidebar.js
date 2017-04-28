65e340698797839a232b85ee343843e439984ede
pkmgarcia <pkmgarcia@yahoo.com>
2017-04-27 21:39:28 -0700

    started work on folders

---
+++ appy/view/profile/sidebar.js
@@ -0,0 +1,11 @@
+function sidebar_open() {
+  document.getElementById("main").style.marginLeft = "25%";
+  document.getElementById("mySidebar").style.width = "25%";
+  document.getElementById("mySidebar").style.display = "block";
+  document.getElementById("openNav").style.display = 'none';
+}
+function sidebar_close() {
+  document.getElementById("main").style.marginLeft = "0%";
+  document.getElementById("mySidebar").style.display = "none";
+  document.getElementById("openNav").style.display = "inline-block";
+}
